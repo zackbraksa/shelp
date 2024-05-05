95fff9cb7278636732587f8b9de8d7f1101af4cb
Zack Braksa <zakaria.braksa@gmail.com>
2024-05-05 01:16:25 +0100

    Autocorrect rubocop offenses

--- spec/shelp_spec.rb
+++ spec/shelp_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe Shelp do
+RSpec.describe(Shelp) do
   it "has a version number" do
-    expect(Shelp::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(true).to eq(true)
+    expect(Shelp::VERSION).not_to(be(nil))
   end
 end
